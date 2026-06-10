2323870982a848bd224c9a4254ca21d19998ecb9
benoush <bellowbenedicta@gmail.com>
2026-06-10 12:56:32 +0000

    new commit

--- src/modules/planAbonnementProprietaire/planAbonnementProprietaireRoute.ts
+++ src/modules/planAbonnementProprietaire/planAbonnementProprietaireRoute.ts
@@ -4,7 +4,6 @@ import { PlanAbonnementProprietaireController } from "./planAbonnementProprietai
 import { PlanAbonnementProprietairePaginationSchema, createPlanAbonnementProprietaireSchema, PlanAbonnementProprietaireIdSchema } from "./planAbonnementProprietaireSchema";
 import { authMiddleware, isAdmin } from "../middleware/auth.middleware";
 import { updatePlanAbonnementClientSchema } from "../planAbonnementClient/planAbonnementClientSchema";
-import is from "zod/v4/locales/is.js";
 
 const router: Router = Router();
 const planAbonnementProprietaireController = new PlanAbonnementProprietaireController();
@@ -14,9 +13,9 @@ const planAbonnementProprietaireController = new PlanAbonnementProprietaireContr
 // router.use(authMiddleware);
 router.post("", validate(createPlanAbonnementProprietaireSchema, "body"), planAbonnementProprietaireController.createPlanAbonnementProprietaire);
 router.get("", validate(PlanAbonnementProprietairePaginationSchema, "query"), planAbonnementProprietaireController.getPlanAbonnementProprietairePaginated);
-router.patch("/type/:id", isAdmin, validate(PlanAbonnementProprietaireIdSchema, "params"), validate(updatePlanAbonnementClientSchema, "body"), planAbonnementProprietaireController.updateType);
+router.patch("/type/:id", validate(PlanAbonnementProprietaireIdSchema, "params"), validate(updatePlanAbonnementClientSchema, "body"), planAbonnementProprietaireController.updateType);
 router.get("/:id", validate(PlanAbonnementProprietaireIdSchema, "params"), planAbonnementProprietaireController.getPlanAbonnementProprietaireById);
-router.delete("/:id", isAdmin, validate(PlanAbonnementProprietaireIdSchema, "params"), planAbonnementProprietaireController.deletePlanAbonnementProprietaire);
-router.patch("/:id", isAdmin, validate(PlanAbonnementProprietaireIdSchema, "params"), validate(createPlanAbonnementProprietaireSchema, "body"), planAbonnementProprietaireController.updatePlanAbonnementProprietaire);
+router.delete("/:id", validate(PlanAbonnementProprietaireIdSchema, "params"), planAbonnementProprietaireController.deletePlanAbonnementProprietaire);
+router.patch("/:id", validate(PlanAbonnementProprietaireIdSchema, "params"), validate(createPlanAbonnementProprietaireSchema, "body"), planAbonnementProprietaireController.updatePlanAbonnementProprietaire);
 
 export default router;
